a0f59d500be75bc0d7de7d61b6d944dfef26e3b3
admin <admin@stu77534b>
2018-10-17 11:40:30 +0800

    frist build

---
+++ src/main/java/com/bwf/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package com.bwf.dao;
+
+
+import com.bwf.entity.User;
+
+
+public interface UserMapper {
+	
+	User getUserById(int id);	
+	
+	void add ( User user );
+	
+	void delete ( int id );
+	
+	void update ( User user );
+
+	User getUserByUsernameAndPassword(User user);
+	
+	
+}
---
+++ src/main/java/com/bwf/entity/User.java
@@ -0,0 +1,66 @@
+package com.bwf.entity;
+
+
+
+public class User {
+
+	private int userId;
+	
+	private String username;
+	
+	private String password;
+	
+	private String nickname;
+	
+	private String avater;
+	
+
+
+	public String getAvater() {
+		return avater;
+	}
+
+	public void setAvater(String avater) {
+		this.avater = avater;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+
+	
+
+	
+}
